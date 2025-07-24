b5ba8cb43b43dc7c8330c967b506ebb79e56d5d1
Antoine <antoine.familleroger@gmail.com>
2025-07-24 14:18:45 +0200

    Working front

--- front/render-front/handler.go
+++ front/render-front/handler.go
@@ -2,9 +2,11 @@ package function
 
 import (
 	_ "embed"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"sync"
 	"time"
@@ -21,14 +23,41 @@ type RenderRequest struct {
 
 type RenderResult struct {
 	S1X     int    `json:"s1x"`
-	ImgData string `json:"img_data"` // base64-encoded or data URL
+	ImgData string `json:"img_data"` // base64-encoded data URL
 }
 
+var (
+	jobResults = make(map[string][]RenderResult)
+	jobStatus  = make(map[string]bool)
+	jobMutex   sync.Mutex
+)
+
 func Handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet && r.URL.Query().Has("job") {
+		// Return job status
+		jobID := r.URL.Query().Get("job")
+		jobMutex.Lock()
+		defer jobMutex.Unlock()
+
+		if !jobStatus[jobID] {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "pending",
+			})
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status": "complete",
+			"images": jobResults[jobID],
+		})
+		return
+	}
+
 	if r.Method == http.MethodGet {
-		// Serve frontend HTML
+		// Serve frontend
 		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, htmlContent)
 		return
 	}
@@ -42,7 +71,20 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	body, _ := io.ReadAll(r.Body)
 	_ = json.Unmarshal(body, &req)
 
-	// Fixed values from original script
+	// Generate a random job ID
+	jobID := fmt.Sprintf("job-%d", rand.Intn(1000000))
+
+	// Start rendering in background
+	go renderJob(jobID, req)
+
+	// Respond with job ID
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"job": jobID,
+	})
+}
+
+func renderJob(jobID string, req RenderRequest) {
 	const (
 		s1y = 20
 		s1z = 50
@@ -51,67 +93,28 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		s2z = 80
 	)
 
-	var wg sync.WaitGroup
-	var mu sync.Mutex
+	client := http.Client{Timeout: 10 * time.Minute}
 	results := []RenderResult{}
 
 	for s1x := req.S1XStart; s1x <= req.S1XEnd; s1x++ {
-		wg.Add(1)
-		go func(s1x int) {
-			defer wg.Done()
+		url := fmt.Sprintf(
+			"http://render.openfaas-fn:8080/render?samples=%d&s1x=%d&s1y=%d&s1z=%d&s2x=%d&s2y=%d&s2z=%d",
+			req.Samples, s1x, s1y, s1z, s2x, s2y, s2z,
+		)
 
-			url := fmt.Sprintf(
-				"http://render.openfaas-fn:8080/render?samples=%d&s1x=%d&s1y=%d&s1z=%d&s2x=%d&s2y=%d&s2z=%d",
-				req.Samples, s1x, s1y, s1z, s2x, s2y, s2z,
-			)
+		resp, err := client.Get(url)
+		if err != nil || resp.StatusCode != 200 {
+			continue
+		}
+		imgData, _ := io.ReadAll(resp.Body)
+		resp.Body.Close()
 
-			client := http.Client{
-				Timeout: 10 * time.Minute,
-			}
-			resp, err := client.Get(url)
-			if err != nil || resp.StatusCode != 200 {
-				return
-			}
-			defer resp.Body.Close()
-			imgData, _ := io.ReadAll(resp.Body)
-
-			// Embed as data URL (you could base64 it if needed)
-			imgURL := "data:image/png;base64," + encodeBase64(imgData)
-
-			mu.Lock()
-			results = append(results, RenderResult{S1X: s1x, ImgData: imgURL})
-			mu.Unlock()
-		}(s1x)
+		dataURL := "data:image/png;base64," + base64.StdEncoding.EncodeToString(imgData)
+		results = append(results, RenderResult{S1X: s1x, ImgData: dataURL})
 	}
 
-	wg.Wait()
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(results)
-}
-
-func encodeBase64(data []byte) string {
-	return fmt.Sprintf("%s", toBase64(data))
-}
-
-func toBase64(data []byte) []byte {
-	encoding := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	dst := make([]byte, ((len(data)+2)/3)*4)
-	for i, j := 0, 0; i < len(data); i += 3 {
-		var b [3]byte
-		copy(b[:], data[i:])
-		dst[j+0] = encoding[b[0]>>2]
-		dst[j+1] = encoding[((b[0]&0x03)<<4)|(b[1]>>4)]
-		if i+1 < len(data) {
-			dst[j+2] = encoding[((b[1]&0x0F)<<2)|(b[2]>>6)]
-		} else {
-			dst[j+2] = '='
-		}
-		if i+2 < len(data) {
-			dst[j+3] = encoding[b[2]&0x3F]
-		} else {
-			dst[j+3] = '='
-		}
-		j += 4
-	}
-	return dst
+	jobMutex.Lock()
+	jobResults[jobID] = results
+	jobStatus[jobID] = true
+	jobMutex.Unlock()
 }
